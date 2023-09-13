adac5722d73afd0ef74fcfc243e6f6de2b2bc854
Aaron Zellweger <aaron.zellweger@gmail.com>
2023-09-13 10:11:32 +0200

    add firebase to project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,23 @@
 import { ApplicationConfig, importProvidersFrom } from "@angular/core";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
 import { BrowserModule } from "@angular/platform-browser";
 import { provideAnimations } from "@angular/platform-browser/animations";
 import { Routes, provideRouter } from "@angular/router";
+import { environment } from '../environments/environment';
 
 const routes: Routes = []
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(BrowserModule),
+    importProvidersFrom(
+      BrowserModule,
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore())
+    ),
     provideAnimations()
   ]
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'trainingsjournal-409a8',
+    appId: '1:188018928417:web:8662f21e89a5fde4c56c60',
+    storageBucket: 'trainingsjournal-409a8.appspot.com',
+    apiKey: 'AIzaSyDwQ5QJ3x9jJng2KzstNYbvlPdxzwHsjMc',
+    authDomain: 'trainingsjournal-409a8.firebaseapp.com',
+    messagingSenderId: '188018928417',
+    measurementId: 'G-JKW7QR0C2R',
+  },
   production: true
 };
